08af50af4f7935d74f406aca9a3c43caa3e284db
Gonzalo Madona <99615258+FireDash22@users.noreply.github.com>
2022-10-02 01:50:55 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 import io from 'socket.io-client';
 
-const socket = io('');
+const socket = io();
 
 function App() {
   const [message, setMessage] = useState('');
